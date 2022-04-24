7e928451e455751e66e20f70e829ba46b5dd9897
Peter <peterjakemiranda@gmail.com>
2022-04-25 00:17:39 +0800

    update autofocus field

--- resources/js/Pages/Auth/Register.vue
+++ resources/js/Pages/Auth/Register.vue
@@ -36,7 +36,7 @@ const submit = () => {
 
             <div class="mt-4">
                 <BreezeLabel for="last_name" value="Last Name" />
-                <BreezeInput id="last_name" type="text" class="mt-1 block w-full" v-model="form.last_name" required autofocus autocomplete="name" />
+                <BreezeInput id="last_name" type="text" class="mt-1 block w-full" v-model="form.last_name" required autocomplete="name" />
             </div>
 
             <div class="mt-4">
